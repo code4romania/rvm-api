db4bd3ad2ce62e4952a6ca1b43a75078bf819b46
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-29 11:07:45 +0200

    Template download fix

--- src/app/Http/Controllers/ResourceController.php
+++ src/app/Http/Controllers/ResourceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 use App\Resource;
 use App\Organisation;
 use App\ResourceCategory;
