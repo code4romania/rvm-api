0c8bb5dc12a6b5a0cddbb14060174f3c2a556506
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-29 20:17:17 +0200

    Html fix: closed tag.

--- src/resources/views/mails/resource_add.blade.php
+++ src/resources/views/mails/resource_add.blade.php
@@ -1,6 +1,6 @@
 <div style="width: 50%; margin: 0 auto;">
   <p style="text-align: center; font-size: 20px;">Adăugare nouă de resurse,</p>
-  <p>Asociația <strong>{{ $data['name'] }}<strong> a efectuat o adăugare la resursele sale din platformă.</p>
+  <p>Asociația <strong>{{ $data['name'] }}</strong> a efectuat o adăugare la resursele sale din platformă.</p>
   <hr>
 
 </div>
--- src/resources/views/mails/volunteer_add.blade.php
+++ src/resources/views/mails/volunteer_add.blade.php
@@ -1,6 +1,6 @@
 <div style="width: 50%; margin: 0 auto;">
   <p style="text-align: center; font-size: 20px;">Adăugare nouă de voluntari,</p>
-  <p>Asociația <strong>{{ $data['name'] }}<strong> a efectuat o adăugare la voluntarii săi din platformă.</p>
+  <p>Asociația <strong>{{ $data['name'] }}</strong> a efectuat o adăugare la voluntarii săi din platformă.</p>
   <hr>
 
 </div>
