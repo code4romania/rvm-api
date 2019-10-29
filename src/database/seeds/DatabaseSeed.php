137fd482824a57ec586a8657bf70a366f2f88508
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-29 08:23:54 +0200

    Removed "Altele" option from resource categories. Removed "UsersTableSeeder" from the default seeding.

--- src/database/seeds/DatabaseSeed.php
+++ src/database/seeds/DatabaseSeed.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeed extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call([
-            CourseNamesTableSeeder::class,
-            InstitutionsTableSeeder::class,
-            ResourceCategoriesTableSeeder::class,
-            CountriesTableSeeder::class,
-            CountiesTableSeeder::class,
-            CitiesTableSeeder::class,
-            UsersTableSeeder::class,
-            StaticsViewsSeeder::class
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    $this->call([
+      CourseNamesTableSeeder::class,
+      InstitutionsTableSeeder::class,
+      ResourceCategoriesTableSeeder::class,
+      CountriesTableSeeder::class,
+      CountiesTableSeeder::class,
+      CitiesTableSeeder::class,
+      StaticsViewsSeeder::class
+    ]);
+  }
 }
