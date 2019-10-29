ebd5e492cfad05887d7ea962642f45b86df34938
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-29 16:10:21 +0200

    Template download fix

--- src/app/Http/Controllers/VolunteerController.php
+++ src/app/Http/Controllers/VolunteerController.php
@@ -674,7 +674,8 @@ class VolunteerController extends Controller
      *                500 if an error occurs
      */
     public function template() {
-      return Storage::disk('public')->download('template-voluntari.csv');
+      return response()->download(storage_path("app/public/template-voluntari.csv"));
+      // return Storage::disk('public')->download('template-voluntari.csv');
       // return Storage::download(public_path('/template-voluntari.csv'), 'template-voluntari.csv');
     }
 }
